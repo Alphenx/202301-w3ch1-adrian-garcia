9ae39d2c64e5b47a8b3c2f7c58483d98dfe09857
Adrian_G <adriangarcia.comunicacion@gmail.com>
2023-01-31 01:06:57 +0100

    Add product rating function

--- public/js/index.js
+++ public/js/index.js
@@ -1,2 +1,7 @@
-export const helloWorld = () => 'Hello!';
-console.log('Hola');
+// export const helloWorld = () => 'Hello!';
+import { outOfStockProducts } from './product-list-stock.js';
+import { productDiscount } from './product-discount.js';
+import { ratingSortedProducts } from './product-rating.js';
+console.log(outOfStockProducts());
+console.log(productDiscount());
+console.log(ratingSortedProducts());
---
+++ src/ts/product-rating.ts
@@ -0,0 +1,14 @@
+import { data } from './data.js';
+
+export const ratingSortedProducts = () => {
+  const sortedProducts: string[] = [];
+
+  for (let i = 0; i < data.products.length; i++) {
+    if (data.products[i].rating > data.products[i - 1].rating) {
+      sortedProducts.push(data.products[i].title);
+    } else if (data.products[i].rating < data.products[i - 1].rating) {
+      sortedProducts.unshift(data.products[i].title);
+    }
+  }
+  return sortedProducts;
+};
